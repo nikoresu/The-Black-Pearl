862d6d0a9615252f63d8e2e06b3b036efd17eac9
Nicolás Montañez Velasco <nicolasmonta1807@gmail.com>
2026-03-02 15:55:53 -0500

    feat: simple script to update services

---
+++ updater.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Updating service images..."
+
+docker compose pull
+
+echo "Recreating containers..."
+
+docker compose up --force-recreate -d
+
+echo "Cleaning old images..."
+
+docker image prune -f
